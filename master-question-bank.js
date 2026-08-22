1a46c682e0b49672f0e80b51f9491069ed53aa6b
musbeigikiwi <mustafabeigi9@gmail.com>
2026-08-23 01:50:13 +1200

    Update master-question-bank.js

--- master-question-bank.js
+++ master-question-bank.js
@@ -3,7 +3,7 @@ window.AM_MASTER_BANK = {
   title: "GEDU404B Test 1 — Master Question Bank",
   chapters: [1, 2, 3, 4],
   mcqCount: 200,
-  shortAnswerCount: 60,
+  shortAnswerCount: 100,
 
   mcqs: [
 
